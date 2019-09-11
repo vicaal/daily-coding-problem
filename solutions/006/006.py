69cb06bb484b82a360ff38fea5dfcf74d92f3707
vicaal <vicaal63@gmail.com>
2019-09-11 11:23:58 -0300

    Removed __main__ method

--- solutions/006/006.py
+++ solutions/006/006.py
@@ -27,23 +27,20 @@ class XORLinkedList:
             result_node = self.memory[next_node_index]
         return result_node
 
+node_a = Node("node_a")
+node_b = Node("node_b")
+node_c = Node("node_c")
+node_d = Node("node_d")
+node_e = Node("node_e")
 
-if __name__ == "__main__":
+l = XORLinkedList(node_d)
+l.add(node_b)
+l.add(node_a)
+l.add(node_c)
+l.add(node_e)
 
-    node_a = Node("node_a")
-    node_b = Node("node_b")
-    node_c = Node("node_c")
-    node_d = Node("node_d")
-    node_e = Node("node_e")
-
-    l = XORLinkedList(node_d)
-    l.add(node_b)
-    l.add(node_a)
-    l.add(node_c)
-    l.add(node_e)
-
-    assert l.get(0).value == "node_d"
-    assert l.get(1).value == "node_b"
-    assert l.get(2).value == "node_a"
-    assert l.get(3).value == "node_c"
-    assert l.get(4).value == "node_e"
+assert l.get(0).value == "node_d"
+assert l.get(1).value == "node_b"
+assert l.get(2).value == "node_a"
+assert l.get(3).value == "node_c"
+assert l.get(4).value == "node_e"
